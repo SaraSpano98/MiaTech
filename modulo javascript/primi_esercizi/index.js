c95e7af849192195180f87d7cc42920a5b0d5526
Sara Spano <saraspano@live.com>
2025-02-28 11:00:50 +0100

    new modulo

---
+++ modulo javascript/primi_esercizi/index.js
@@ -0,0 +1,15 @@
+const myName = "Sara";
+const age = 26;
+const isStudente = true;
+
+console.log(myName); 
+console.log(age);
+console.log(isStudente);
+
+console.log(myName, age, isStudente);
+
+const messaggio = "Ciao, mi chiamo " + myName + " e ho " + age + " anni.";
+console.log(messaggio);
+
+const newMessage = `Ciao, mi chiamo ${myName} e ho ${age} anni`;
+console.log(newMessage);
