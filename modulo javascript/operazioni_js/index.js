181e803358960537789ef0c640b6e6cba695fac0
Sara Spano <saraspano@live.com>
2025-03-02 12:07:02 +0100

    operations in Javascript

---
+++ modulo javascript/operazioni_js/index.js
@@ -0,0 +1,47 @@
+//somma di due numeri
+let a = 120;
+let b = 1200;
+
+console.log("la somma è:", a + b);
+
+
+//Chiedi due numeri all'utente (prompt())
+let num1 = Number(prompt("inserisci il primo numero:"));
+let num2 = Number(prompt("inserisci il seconodo numero:"));
+
+let sum = num1 + num2;
+
+console.log("La somma è:", somma);
+
+
+//Converti i valori in numeri e stampane la somma
+let x = 125;
+let y = 300;
+let z = 98;
+
+let somma = x + y + z;
+console.log(somma); // Stampa 523
+
+//Chiedi un numero all'utente e usa il modulo % per verificare se è pari o dispari.
+let numero = 125;  // Puoi cambiare questo valore per testare altri numeri
+
+if (numero % 2 === 0) {
+    console.log(numero + " è un numero pari.");
+} else {
+    console.log(numero + " è un numero dispari.");
+}
+
+
+// Chiedi all'utente di inserire la sua età
+let eta = prompt("Inserisci la tua età:");
+let age = 23; 
+console.log("Hai" + age + "anni");
+
+// Verifica se l'età è inferiore a 18 (minorenne) o maggiore o uguale a 18 (maggiorenne)
+if (age < 18) {
+    console.log("Sei minorenne.");
+} else {
+    console.log("Sei maggiorenne.");
+}
+
+
