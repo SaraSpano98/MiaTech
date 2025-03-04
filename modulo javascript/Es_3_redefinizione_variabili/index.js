b45362c2c0a0a79c6f91d29d2475c1fb77310905
Sara Spano <saraspano@live.com>
2025-03-04 11:20:57 +0100

    added new exercise

---
+++ modulo javascript/Es_3_redefinizione_variabili/index.js
@@ -0,0 +1,8 @@
+let number = 42; 
+
+{
+    let number = 68;
+    console.log(number);  //PER PRIMO, stampa il valore all'interno del blocco 
+}
+
+console.log(number); //PER SECONDO, stampa il valore fuori del blocco
