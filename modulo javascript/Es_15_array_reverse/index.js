d88dc28c9d0ff296b1a746bb2018dea05b340a4b
Sara Spano <saraspano@live.com>
2025-03-06 14:30:02 +0100

    added exercise

---
+++ modulo javascript/Es_15_array_reverse/index.js
@@ -0,0 +1,8 @@
+const num = ["1", "2", "3"];
+let numLen = num.length;
+
+for(let i = num.length - 1; i >= 0; i--) {
+    reversedArray.push(num [i]); // Aggiunge gli elementi in ordine inverso
+}
+
+console.log("Array invertito:", reversedArray);
