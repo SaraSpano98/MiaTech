c1799f762f45ce2b2e4ea0b23384325eb2ef9526
Sara Spano <saraspano@live.com>
2025-03-08 08:39:24 +0100

    exercise done to add

---
+++ modulo javascript/Es_26_object_values/index.js
@@ -0,0 +1,10 @@
+const student = {
+    name: "Charlotte",
+    age: 21,
+    grade: "fourth",
+    school: "Harvard University "
+};
+
+const values = Object.values(student);
+
+console.log(values);
