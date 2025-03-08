05a651dd427b6a6880873a4531050a927f34a369
Sara Spano <saraspano@live.com>
2025-03-08 10:09:45 +0100

    exercise done to add

---
+++ modulo javascript/Es_28_foreach_e_map/index.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach(myFunction);
+
+function myFunction(num) {
+    return num * 10;
+};
+
+console.log(numbers);
+
+const squaredNumbers = numbers.map(myFunction);
+console.log(squaredNumbers); 
