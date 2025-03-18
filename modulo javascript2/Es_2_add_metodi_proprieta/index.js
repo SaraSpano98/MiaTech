048fb6b37671a5f7aa4bf7ce2211936c232672a4
Sara Spano <saraspano@live.com>
2025-03-18 10:05:05 +0100

    add new exercise done

--- modulo javascript2/Es_2_add_metodi_proprieta/index.js
+++ modulo javascript2/Es_2_add_metodi_proprieta/index.js
@@ -3,10 +3,27 @@ class Automobile {
     this.marca = marca; 
     this.modello = modello;
     this.anno = anno; 
+    this.chilometraggio = chilometraggio;
     }
+
     descrizione() {
         return `Automobile: ${this.marca} ${this.modello}, Anno: ${this.anno}`; 
     }
+
+    aggiungiChilometri(km) {
+        if (km > 0) {
+            this.chilometraggio += km;
+        } else {
+            console.log("Il valore dei chilometri deve essere positivo.");
+        }
+    }
+
+    mostraChilometraggio() {
+        return `Chilometraggio attuale: ${this.chilometraggio} km`;
+    }
 }
+
 const auto = new Automobile("Fiat", 2016, "Grande Punto");
+auto.aggiungiChilometri(150);
 console.log(auto.descrizione());
+console.log(mostraChilometraggio());
