3ae4166d2b76b52772077c9a15e40ee693ab303d
Sara Spano <saraspano@live.com>
2025-03-20 15:29:57 +0100

    add this exercise done

---
+++ modulo javascript3/Es_4_estrazione_proprieta/index.js
@@ -0,0 +1,11 @@
+const persona = {
+    nome: "Mario",
+    cognome: "Rossi",
+    eta: 30,
+    città: "Roma"
+};
+
+const { nome, eta } = persona;
+
+console.log("Nome:", nome);
+console.log("Età:", eta);
