daefd9ff5b959faa532219bae53aa297ae1a771c
Sara Spano <saraspano@live.com>
2025-03-20 13:31:07 +0100

    esercise done to add

---
+++ modulo javascript3/Es_3_destructuring_valori_diversi/index.js
@@ -0,0 +1,9 @@
+const numeri = [10, 20, 30];
+
+const [primo = 0, secondo = 0, terzo = 0, quarto = 0, quinto = 0] = numeri;
+
+console.log("Primo numero:", primo);
+console.log("Secondo numero:", secondo);
+console.log("Terzo numero:", terzo);
+console.log("Quarto numero:", quarto);
+console.log("Quinto numero:", quinto);
