5bf3f8e8987b0c5985d1a6fc9d57c1f29c1797b2
Sara Spano <saraspano@live.com>
2025-03-25 16:04:17 +0100

    exercise done

---
+++ modulo javascript3/Es_13_conversione_oggetto_json/index.js
@@ -0,0 +1,17 @@
+const topTenVincitori = {
+    primo: "Danilo",
+    secondo: "Alessia",
+    terzo: "Clara",
+    quarto: "Gioanna",
+    quinto: "Lorenzo",
+    sesto: "Paola",
+    settimo: "Riccardo",
+    ottavo: "Eleonora",
+    nono: "Roberto",
+    decimo: "Luca",
+};
+
+// Conversione della stringa JSON in un oggetto JavaScript
+const jsonString = JSON.stringify(topTenVincitori);
+
+console.log(jsonString);
