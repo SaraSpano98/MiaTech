22321a7156f3b909aaa3fba4824c9eb275a752cc
Sara Spano <saraspano@live.com>
2025-03-26 11:34:45 +0100

    exercise done to add

---
+++ modulo javascript3/Es_19_utilizzo_consolelog/index.js
@@ -0,0 +1,38 @@
+let numero = 42; 
+let stringa = "Ciao come va?";
+let array = [10, 22, 88, 74, 54, 41];
+let oggetto = {nome:"Sara", cognome: "Spano", eta: 27};
+let booleano = true || false;
+let nullo = null;
+let indefinito; 
+let stringaVuota = "";
+let arrayVuoto = [];
+let oggettoVuoto = {};
+let grandeNumero = 121354654651321354654n;
+let simbolo = Symbol("id");
+function test () {};
+let data = new Date()
+
+console.log(numero);
+console.log(stringa);
+console.log(array);
+console.log(oggetto);
+console.log(booleano);
+console.log(nullo);
+console.log(indefinito);
+console.log(stringaVuota, arrayVuoto, oggettoVuoto);
+console.log(grandeNumero);
+console.log(simbolo);
+console.log(test);
+console.log(data);
+
+//CONSOLE.LOG:  è una funzione di JavaScript che stampa qualcosa nella console del browser o nel terminal.
+//È uno degli strumenti di debugging più comuni, usato per visualizzare il valore di variabili, monitorare il flusso del programma, 
+// o verificare se il codice funziona correttamente.
+
+//COSA FA: 
+//1. STAMPA INFORMAZIONI: Può stampare valori di variabili, messaggi di testo, oggetti, array e molto altro.
+//2. AIUTA A FARE IL DEBUG: Permette di monitorare cosa succede nel codice durante l'esecuzione, così puoi identificare e correggere errori.
+//3. VISUALIZZA L'OUTPUT: Mostra il risultato direttamente nella console, che si trova nel browser o nell'ambiente di sviluppo. 
+
+
