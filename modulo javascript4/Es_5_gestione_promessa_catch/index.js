d7decbbf61daeb6c68388f055d046bd3552ce9f1
Sara Spano <saraspano@live.com>
2025-04-02 11:43:50 +0200

    file ok

--- modulo javascript4/Es_5_gestione_promessa_catch/index.js
+++ modulo javascript4/Es_5_gestione_promessa_catch/index.js
@@ -1,7 +1,12 @@
 function operazione() {
-    return new Promise((reject) => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
-            reject("Errore durante l'operazione!"); // Simuliamo un errore
+            let successo = Math.random() > 0.5; // Genera un valore casuale true/false
+            if (successo) {
+                resolve("Operazione completata dopo 2 secondi")
+            } else {
+                reject("Operazione fallita: si è verificato un errore!")
+            }
         }, 2000);
     });
 }
@@ -13,3 +18,7 @@ operazione()
 //resolve(...) è una funzione che viene chiamata quando la promise si completa con successo
 //Dopo aver creato una Promise, possiamo eseguirla e usare .then() per ottenere il valore quando la promessa viene risolta.
 //Se una promessa fallisce, possiamo gestire l'errore con .catch().
+
+//CONSIDERAZIONI:
+//alla parte dell'if(): math.random > 0.5 simula un successo o un fallimento in moodo casuale
+//A riguardo di questa parte (sopra), la Promise risolve o rifiuta il tutto dopo 2 secondi in modo casuale. 
