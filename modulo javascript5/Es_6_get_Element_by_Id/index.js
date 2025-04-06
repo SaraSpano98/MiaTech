9fec5cdd285d6310ae152dea61db7ca77c9eb20f
Sara Spano <saraspano@live.com>
2025-04-06 18:01:39 +0200

    add lines in js

--- modulo javascript5/Es_6_get_Element_by_Id/index.js
+++ modulo javascript5/Es_6_get_Element_by_Id/index.js
@@ -6,3 +6,17 @@ button.addEventListener('click', function() {
     paragrafo.textContent = "Il testo del paragrafo è stato cambiato!";
     console.log('Testo del paragrafo cambiato');
 });
+
+//SPIEGAZIONE:
+//document.getElementById() è un metodo JavaScript che serve per selezionare un elemento HTML tramite il suo attributo id.
+
+//COSA FA?
+//RESTITUISCE il primo elemento della pagina che ha l'id indicato.
+//Se non trova nulla, RESTITUISCE NULL.
+
+//ATTENZIONE!: 
+//Ogni ID nella pagina dovrebbe essere unico. Se ci sono più elementi con lo stesso id, getElementbyId PRENDERA' SOLO IL PRIMO!
+
+//IN BREVE:
+//getElementById("id"): non usa selettore css, restituisce un singolo elemento, e bisogna usarlo quando si conosce esattamente l'id
+//querySelector("#id"): usa selettore css, restituisce un singolo elemento, e bisogna usarlo perchè è più flessibile per selettori CSS
