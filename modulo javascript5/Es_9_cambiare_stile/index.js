740121e60dfcc197dea851b98874d107987d4aef
Sara Spano <saraspano@live.com>
2025-04-06 18:34:44 +0200

    add done exercise

---
+++ modulo javascript5/Es_9_cambiare_stile/index.js
@@ -0,0 +1,13 @@
+function cambiaStile() {
+    const paragrafo = document.getElementById("paragrafo");
+
+    paragrafo.style.backgroundColor = "red";
+    paragrafo.style.fontSize = "25px";
+    paragrafo.style.color = "white";
+    console.log(paragrafo);
+}
+
+//Attenzione: le proprietà CSS con trattini (background-color) si scrivono in camelCase (backgroundColor) in JavaScript.
+
+
+
