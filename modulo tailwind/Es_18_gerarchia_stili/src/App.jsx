2927d56289ac8fc5f69d74dce5af6ff979e3ff35
Sara Spano <saraspano@live.com>
2025-04-18 10:43:09 +0200

    exercise done to add

---
+++ modulo tailwind/Es_18_gerarchia_stili/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+
+  return (
+    <div className="p-10 space-y-6">
+      <h1 className="heading-1">Titolo Principale</h1>
+      <h2 className="heading-2">Sottotitolo Importante</h2>
+      <p className="paragraph">
+        Questo è un paragrafo che dimostra l'utilizzo di stili personalizzati in Tailwind.
+        La gerarchia visiva è fondamentale per una buona esperienza utente.
+      </p>
+
+      <div className="space-x-4">
+        <button className="button-primary">Azione Principale</button>
+        <button className="button-secondary">Azione Secondaria</button>
+      </div>
+    </div>
+  );
+
+}
+
+export default App
