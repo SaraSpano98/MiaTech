9a2179d464c5123ef48fbdb655851cacbb941ce4
Sara Spano <saraspano@live.com>
2025-04-28 16:31:20 +0200

    exercise completed

---
+++ modulo react/Es_9_reset_contatore/src/Counter.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0); // inizializza lo stato a 0
+
+  const increment = () => {
+    setCount(count + 1); // aumenta di 1
+  };
+
+  const decrement = () => {
+    setCount(count - 1); // diminuisce di 1
+  };
+
+  const reset = () => {
+    setCount(0); // resetta il contatore a 0
+  };
+
+  return (
+    <div>
+      <div>Count: {count}</div>
+      <button onClick={increment}>Incrementa</button>
+      <button onClick={decrement}>Decrementa</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default Counter;
+
