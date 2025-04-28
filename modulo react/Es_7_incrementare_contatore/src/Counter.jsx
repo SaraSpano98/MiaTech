8ce4733f1f7022d25d820aabc358a587b05b4630
Sara Spano <saraspano@live.com>
2025-04-28 15:48:16 +0200

    exercise completed

---
+++ modulo react/Es_7_incrementare_contatore/src/Counter.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0); // inizializza lo stato a 0
+
+  const increment = () => {
+    setCount(count + 1); // aumenta il valore di count di 1
+  };
+
+  return (
+    <div>
+      <div>Count: {count}</div>
+      <button onClick={increment}>Incrementa</button>
+    </div>
+  );
+}
+
+export default Counter;
+
