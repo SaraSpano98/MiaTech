4ed9fe133090b15b0191ddc2a3348fd197c71c2b
Sara Spano <saraspano@live.com>
2025-04-29 17:31:48 +0200

    exercise completed

---
+++ modulo react/Es_18_uso_props_children_layout/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './Card';
+
+function App() {
+  return (
+    <div>
+      <Card>
+        <h2>Benvenuto</h2>
+        <p>Questo è un messaggio dentro la card.</p>
+      </Card>
+    </div>
+  );
+}
+
+export default App;
