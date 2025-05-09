8fe72b93ba1975be215fa3319be6e4e04611b921
Sara Spano <saraspano@live.com>
2025-05-09 10:56:39 +0200

    fixed exercises

--- modulo react/Es_28_integrare_TodoContext/src/App.jsx
+++ modulo react/Es_28_integrare_TodoContext/src/App.jsx
@@ -1,32 +1,13 @@
-import React from 'react';
-import { useState, useContext } from 'react';
-import { TodoProvider, TodoContext } from './TodoContext.jsx';
+import { TodoProvider } from './TodoContext.jsx';
 import TodoList from './TodoList.jsx';
 
 const App = () => {
-  const [newTodo, setNewTodo] = useState('');
-  const { addTodo } = useContext(TodoContext);
+  
 
   return (
     <TodoProvider>
       <div>
         <h1>Lista To-Do</h1>
-        <input
-          type="text"
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
-          placeholder="Aggiungi un nuovo to-do"
-        />
-        <button
-          onClick={() => {
-            if (newTodo) {
-              addTodo(newTodo);  // Aggiungi il nuovo to-do
-              setNewTodo('');
-            }
-          }}
-        >
-          Aggiungi
-        </button>
         <TodoList />
       </div>
     </TodoProvider>
--- modulo react/Es_28_integrare_TodoContext/src/TodoList.jsx
+++ modulo react/Es_28_integrare_TodoContext/src/TodoList.jsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import { useContext } from 'react';
-import { TodoContext } from './TodoContext.jsx';
+import React from "react";
+import { useContext, useState } from "react";
+import { TodoContext } from "./TodoContext.jsx";
 
 const TodoList = () => {
   const { todos, toggleComplete } = useContext(TodoContext);
 
+  const [newTodo, setNewTodo] = useState('');
+  
+  const {addTodo} = useContext(TodoContext)
+
   return (
-    <ul>
-      {todos.map((todo) => (
-        <li key={todo.id}>
-          <span
-            style={{
-              textDecoration: todo.completed ? 'line-through' : 'none',
-            }}
-            onClick={() => toggleComplete(todo.id)}
-          >
-            {todo.text}
-          </span>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Aggiungi un nuovo to-do"
+      />
+      <button
+        onClick={() => {
+          if (newTodo) {
+            addTodo(newTodo); // Aggiungi il nuovo to-do
+            setNewTodo("");
+          }
+        }}
+      >
+        Aggiungi
+      </button>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+              onClick={() => toggleComplete(todo.id)}
+            >
+              {todo.text}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
