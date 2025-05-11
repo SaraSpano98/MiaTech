eb17979a18ee4b1891fca24f8c59da72de369932
Sara Spano <saraspano@live.com>
2025-05-11 11:57:59 +0200

    pages navigation example

---
+++ modulo react/Es_30_navigazione_pagine/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import { TodoProvider } from "./TodoContext";
+
+const App = () => {
+  return (
+    <TodoProvider>
+      <Router>
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </TodoProvider>
+  );
+};
+
+export default App;
